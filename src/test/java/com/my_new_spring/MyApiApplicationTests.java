40eea16e95ac100c77bba67bddb625acc8e15728
RuthT <87043497+BlaBlue1@users.noreply.github.com>
2022-06-29 22:53:33 -0400

    Add files via upload

---
+++ src/test/java/com/my_new_spring/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_new_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
